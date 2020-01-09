e5e7f1c13dd640be36cad14696ba7ec3b1eaacdb
Michelle Ranly <meranly8@gmail.com>
2020-01-09 18:44:29 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,17 +14,5 @@ const codes = [
 let index = 0;
 
 function init() {
-  document.body.addEventListenter('keydown', function(e) {
-    const key = parseInt(e.key || e.detail);
-    
-    if (codes[index] === key) {
-      index++;
-      
-      if (index = codes.length) {
-        alert('You have done it!');
-      }
-    } else {
-      index = 0;
-    }
-  });
+
 }
